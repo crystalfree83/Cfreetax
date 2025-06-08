7fc6f91334cfc5b7902ff9026875a3667c29eff0
crystalfree83 <crystalfree83@gmail.com>
2025-06-08 02:10:43 -0500

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -67,7 +67,7 @@ const HomePage = () => {
             animate={{ opacity: 1, x: 0 }}
             transition={{ delay: 0.3, duration: 0.5 }}
           >
-            <img  alt="Crystal Free, Enrolled Agent" className="rounded-lg shadow-xl w-full max-w-md mx-auto md:mx-0" src="https://drive.google.com/file/d/12TW_rZQkJlF-CvgxgtVV9_Xkl0Or7fFt/view?usp=drivesdk" />
+            <img  alt="Crystal Free, Enrolled Agent" className="rounded-lg shadow-xl w-full max-w-md mx-auto md:mx-0" src="https://photos.app.goo.gl/4t3Bvf4PxWJs9qpeA" />
           </motion.div>
           <motion.div
             initial={{ opacity: 0, x: 20 }}
